daf9780c065e0e07a92835e0303e424e1e860488
Travis Vocino <travis@vocino.com>
2023-05-13 10:23:17 -0700

    Convert to env variables

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const commands = require('./commands');
 
-const clientId = 'YOUR_CLIENT_ID'; // Replace with your bot's client ID
-const guildId = 'YOUR_GUILD_ID'; // Replace with your guild ID
-const token = 'YOUR_DISCORD_BOT_TOKEN'; // Replace with your bot token
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.BOT_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
